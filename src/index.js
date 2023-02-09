b874b882c09c6d9c540a1b0788c47f7997c43ffd
MuhammadAbuBakarr <bakarkrl29@gmail.com>
2023-02-09 14:57:52 +0500

    adjusting with Prettier

--- src/index.js
+++ src/index.js
@@ -8,28 +8,27 @@ import { MantineProvider } from "@mantine/core";
 import { UserProvider } from "./contexts/UserContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<MantineProvider
-				theme={{
-					colors: {
-						primary: "#1D203E",
-						primaryHover: "rgba(29, 32, 62, 0.8)",
-						background: "#E7E7E780",
-						container: "white",
-						gray: "#E7E7E780",
-						heading: "#56606E",
-						grayDark: "rgb(0,0,0,0.5)",
-					},
-					primaryColor: "blue",
-				}}
-			>
-				<UserProvider>
-					<App />
-				</UserProvider>
-			</MantineProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+ <React.StrictMode>
+  <BrowserRouter>
+   <MantineProvider
+    theme={{
+     colors: {
+      primary: "#1D203E",
+      primaryHover: "rgba(29, 32, 62, 0.8)",
+      background: "#E7E7E780",
+      container: "white",
+      gray: "#E7E7E780",
+      heading: "#56606E",
+      grayDark: "rgb(0,0,0,0.5)",
+     },
+     primaryColor: "blue",
+    }}>
+    <UserProvider>
+     <App />
+    </UserProvider>
+   </MantineProvider>
+  </BrowserRouter>
+ </React.StrictMode>
 );
 
 reportWebVitals();
