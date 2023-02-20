2a288e019cabc26da15ee7995991d5870e293470
Muhammad Usama <mosama4u@gmail.com>
2023-02-20 08:43:41 +0500

    Appointment step 1, 2

--- src/Pages/Appointments/AddAppointment/Step3/index.jsx
+++ src/Pages/Appointments/AddAppointment/Step3/index.jsx
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
-import { SimpleGrid, Container, Flex, Text, useMantineTheme } from "@mantine/core";
+import {
+  SimpleGrid,
+  Container,
+  Flex,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 import { useStyles } from "../styles";
 import DoubleTabs from "./Tabs";
-const Step3 = ({selectedUser}) => {
- return (
-  <Container size="lg">
-   {/* <Text fz={20} fw="bolder" align="center" mb={"md"}>
-    Upload Report
-   </Text> */}
-   <Flex justify={"space-between"}>
-    <SimpleGrid cols={2}>
-     <Text fz={18} fw={"bold"}>
-      Case#
-     </Text>
-     <Text>XXXX</Text>
-    </SimpleGrid>
-    <Flex align={"center"}>
-     <Text fz={18} fw={"bold"}>
-      Date:
-     </Text>
-     <Text ml={10}>XXXX</Text>
-    </Flex>
-   </Flex>
-   {/* ///// */}
-   <DoubleTabs selectedUser={selectedUser}/>
-  </Container>
- );
+const Step3 = ({ selectedUser, caseNo }) => {
+  return (
+    <Container size="lg">
+      <Flex justify={"space-between"}>
+        <SimpleGrid cols={2}>
+          <Text fz={18} fw={"bold"}>
+            Case#
+          </Text>
+          <Text>{caseNo}</Text>
+        </SimpleGrid>
+        <Flex align={"center"}>
+          <Text fz={18} fw={"bold"}>
+            Date:
+          </Text>
+          <Text ml={10}>XXXX</Text>
+        </Flex>
+      </Flex>   
+      <DoubleTabs selectedUser={selectedUser} />
+    </Container>
+  );
 };
 
 export default Step3;
