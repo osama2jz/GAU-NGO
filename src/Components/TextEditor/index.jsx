88c8c91909c89dace5479dcf6e702f6fe43000ce
Muhammad Usama <mosama4u@gmail.com>
2023-02-28 18:17:12 +0500

    text editor changed

--- src/Components/TextEditor/index.jsx
+++ src/Components/TextEditor/index.jsx
@@ -1,13 +1,95 @@
-import { Editor } from "react-draft-wysiwyg";
+import { RichTextEditor, Link } from "@mantine/tiptap";
+import { useEditor } from "@tiptap/react";
+import Highlight from "@tiptap/extension-highlight";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+import TextAlign from "@tiptap/extension-text-align";
+import Superscript from "@tiptap/extension-superscript";
+import SubScript from "@tiptap/extension-subscript";
+import { createStyles } from "@mantine/core";
 
-const TextEditor = ({value, onChange }) => {
+const content = "";
+const useStyles = createStyles((theme) => ({
+  root: {
+    backgroundColor: "transparent",
+    borderBottom: "1px solid rgb(0, 0, 0, 0.1)",
+    color: "black !important",
+    // borderRadius: "5px",
+  },
+  root: {
+    border: "1px solid rgb(0, 0, 0, 0.1)",
+  },
+  content:{
+    minHeight:"300px"
+  }
+}));
+const TextEditor = () => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Underline,
+      Link,
+      Superscript,
+      SubScript,
+      Highlight,
+      TextAlign.configure({ types: ["heading", "paragraph"] }),
+    ],
+    content,
+  });
+
+  const { classes, cx } = useStyles();
   return (
-    <Editor
-      editorState={value}
-      wrapperClassName="wrapperClassName"
-      editorClassName="editorClassName"
-      onEditorStateChange={onChange}
-    />
+    <RichTextEditor
+      editor={editor}
+      classNames={{
+        root: classes.root,
+        toolbar: classes.root,
+        content: classes.content,
+      }}
+    >
+      <RichTextEditor.Toolbar sticky stickyOffset={60}>
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Bold />
+          <RichTextEditor.Italic />
+          <RichTextEditor.Underline />
+          <RichTextEditor.Strikethrough />
+          <RichTextEditor.ClearFormatting />
+          <RichTextEditor.Highlight />
+          <RichTextEditor.Code />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.H1 />
+          <RichTextEditor.H2 />
+          <RichTextEditor.H3 />
+          <RichTextEditor.H4 />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Blockquote />
+          <RichTextEditor.Hr />
+          <RichTextEditor.BulletList />
+          <RichTextEditor.OrderedList />
+          <RichTextEditor.Subscript />
+          <RichTextEditor.Superscript />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.Link />
+          <RichTextEditor.Unlink />
+        </RichTextEditor.ControlsGroup>
+
+        <RichTextEditor.ControlsGroup>
+          <RichTextEditor.AlignLeft />
+          <RichTextEditor.AlignCenter />
+          <RichTextEditor.AlignJustify />
+          <RichTextEditor.AlignRight />
+        </RichTextEditor.ControlsGroup>
+      </RichTextEditor.Toolbar>
+
+      <RichTextEditor.Content />
+    </RichTextEditor>
   );
 };
+
 export default TextEditor;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
