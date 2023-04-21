44cb7d9cbbcd14acd78d401b837d81240e7f425d
Muhammad Usama <mosama4u@gmail.com>
2023-04-21 15:18:01 +0500

    link chanegd

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const backendUrl = "https://backend-gau.herokuapp.com";
+export const backendUrl = "https://gau.usquaresolutions.com";
 export const s3Config = {
   region: "us-east-1",
   IdentityPoolId: "us-east-1:e263637a-572f-406f-884d-82e5ceadf641",
@@ -31,4 +31,3 @@ export const slots = [
   { value: "22:00-23:00", label: "22:00-23:00" },
   { value: "23:00-24:00", label: "23:00-24:00" },
 ];
-
