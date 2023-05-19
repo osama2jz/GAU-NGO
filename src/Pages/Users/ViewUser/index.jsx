7458c6cd62fd71e1c49b6e2f0495a58e6987ff59
UroojMurtaz <uroojmurtaza12@gmail.com>
2023-05-19 16:25:58 +0500

    SideBar

--- src/Pages/Users/ViewUser/index.jsx
+++ src/Pages/Users/ViewUser/index.jsx
@@ -279,7 +279,7 @@ function ViewUser() {
           <Tabs.Tab value="4" onClick={() => setActiveTab(4)}>
             {translate("Primary Document")}
           </Tabs.Tab>
-          {(user.role === "Psychologist" || user.role === "admin" ) && formsData && (
+          {(user.role === "Psychologist" || user.role === "ngoadmin" ) && formsData && (
             <Tabs.Tab value="5" onClick={() => setActiveTab(5)}>
               {translate("Forms")}
             </Tabs.Tab>
